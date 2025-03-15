e00e9e42b626083ce1a122689d871cfdbebe5a54
JustToxical <justtoxical3940can2023@gmail.com>
2025-03-15 13:36:38 -0400

    Update LevelAuthors.js

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -16,7 +16,7 @@ export default {
     template: `
         <div class="level-authors">
             <template v-if="selfVerified">
-                <div class="type-title-sm">Creator & Verifier</div>
+                <div class="type-title-sm">Creator & First Victor</div>
                 <p class="type-body">
                     <span>{{ author }}</span>
                 </p>
@@ -26,7 +26,7 @@ export default {
                 <p class="type-body">
                     <span>{{ author }}</span>
                 </p>
-                <div class="type-title-sm">Verifier</div>
+                <div class="type-title-sm">First Victor</div>
                 <p class="type-body">
                     <span>{{ verifier }}</span>
                 </p>
@@ -35,11 +35,11 @@ export default {
                 <div class="type-title-sm">Creators</div>
                 <p class="type-body">
                     <template v-for="(creator, index) in creators" :key="\`creator-\$\{creator\}\`">
-                        <span >{{ creator }}</span
-                        ><span v-if="index < creators.length - 1">, </span>
+                        <span>{{ creator }}</span>
+                        <span v-if="index < creators.length - 1">, </span>
                     </template>
                 </p>
-                <div class="type-title-sm">Verifier</div>
+                <div class="type-title-sm">First Victor</div>
                 <p class="type-body">
                     <span>{{ verifier }}</span>
                 </p>
