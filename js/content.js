1537b933278cbddcac69554a7cee49b684d1c845
JustToxical <justtoxical3940can2023@gmail.com>
2025-03-15 13:30:18 -0400

    Update content.js

--- js/content.js
+++ js/content.js
@@ -58,15 +58,15 @@ export async function fetchLeaderboard() {
         }
 
         // Verification
-        const firstvictor = Object.keys(scoreMap).find(
-            (u) => u.toLowerCase() === level.firstvictor.toLowerCase(),
-        ) || level.firstvictor;
-        scoreMap[firstvictor] ??= {
-            firstvictor: [],
+        const verifier = Object.keys(scoreMap).find(
+            (u) => u.toLowerCase() === level.verifier.toLowerCase(),
+        ) || level.verifier;
+        scoreMap[verifier] ??= {
+            verified: [],
             completed: [],
             progressed: [],
         };
-        const { firstvictor } = scoreMap[firstvictor];
+        const { verified } = scoreMap[verifier];
         verified.push({
             rank: rank + 1,
             level: level.name,
@@ -80,7 +80,7 @@ export async function fetchLeaderboard() {
                 (u) => u.toLowerCase() === record.user.toLowerCase(),
             ) || record.user;
             scoreMap[user] ??= {
-                firstvictor: [],
+                verified: [],
                 completed: [],
                 progressed: [],
             };
@@ -107,8 +107,8 @@ export async function fetchLeaderboard() {
 
     // Wrap in extra Object containing the user and total score
     const res = Object.entries(scoreMap).map(([user, scores]) => {
-        const { firstvictor, completed, progressed } = scores;
-        const total = [firstvictor, completed, progressed]
+        const { verified, completed, progressed } = scores;
+        const total = [verified, completed, progressed]
             .flat()
             .reduce((prev, cur) => prev + cur.score, 0);
 
