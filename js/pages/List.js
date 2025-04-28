c337a4be5090d8fe3b186ab5634cb9695cd73eab
JustToxical <justtoxical3940can2023@gmail.com>
2025-04-28 18:58:50 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -22,38 +22,20 @@ export default {
         </main>
         <main v-else class="page-list">
             <div class="list-container">
-                <div class="list-section">
-                    <table class="list" v-if="rankedList.length">
-                        <tr v-for="([level, err], i) in rankedList" :key="i">
-                            <td class="rank">
-                                <p class="type-label-lg">#{{ i + 1 }}</p>
-                            </td>
-                            <td class="level" :class="{ 'active': selected == i, 'error': !level }">
-                                <button @click="selectRanked(i)">
-                                    <span class="type-label-lg">{{ level?.name || `Error (${err}.json)` }}</span>
-                                </button>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-
-                <div class="legacy-section" v-if="legacyList.length">
-                    <div class="legacy-title">Legacy</div>
-                    <div class="legacy-levels">
-                        <div
-                            class="level"
-                            v-for="([level, err], j) in legacyList"
-                            :class="{ 'active': selected == j + 150, 'error': !level }"
-                            :key="j"
-                        >
-                            <button @click="selectLegacy(j)">
-                                <span class="type-label-lg">{{ level?.name || `Error (${err}.json)` }}</span>
+                <table class="list" v-if="list">
+                    <tr v-for="([level, err], i) in list">
+                        <td class="rank">
+                            <p v-if="i + 1 <= 150" class="type-label-lg">#{{ i + 1 }}</p>
+                            <p v-else class="type-label-lg">Legacy</p>
+                        </td>
+                        <td class="level" :class="{ 'active': selected == i, 'error': !level }">
+                            <button @click="selected = i">
+                                <span class="type-label-lg">{{ level?.name || \`Error (\${err}.json)\` }}</span>
                             </button>
-                        </div>
-                    </div>
-                </div>
+                        </td>
+                    </tr>
+                </table>
             </div>
-
             <div class="level-container">
                 <div class="level" v-if="level">
                     <h1>{{ level.name }}</h1>
@@ -62,7 +44,7 @@ export default {
                     <ul class="stats">
                         <li>
                             <div class="type-title-sm">Points when completed</div>
-                            <p>{{ score(selectedRank + 1, 100, level.percentToQualify) }}</p>
+                            <p>{{ score(selected + 1, 100, level.percentToQualify) }}</p>
                         </li>
                         <li>
                             <div class="type-title-sm">ID</div>
@@ -74,8 +56,8 @@ export default {
                         </li>
                     </ul>
                     <h2>Records</h2>
-                    <p v-if="selectedRank + 1 <= 75"><strong>{{ level.percentToQualify }}%</strong> or better to qualify</p>
-                    <p v-else-if="selectedRank + 1 <= 150"><strong>100%</strong> or better to qualify</p>
+                    <p v-if="selected + 1 <= 75"><strong>{{ level.percentToQualify }}%</strong> or better to qualify</p>
+                    <p v-else-if="selected +1 <= 150"><strong>100%</strong> or better to qualify</p>
                     <p v-else>This level does not accept new records.</p>
                     <table class="records">
                         <tr v-for="record in level.records" class="record">
@@ -86,7 +68,7 @@ export default {
                                 <a :href="record.link" target="_blank" class="type-label-lg">{{ record.user }}</a>
                             </td>
                             <td class="mobile">
-                                <img v-if="record.mobile" :src="`/assets/phone-landscape${store.dark ? '-dark' : ''}.svg`" alt="Mobile">
+                                <img v-if="record.mobile" :src="\`/assets/phone-landscape\${store.dark ? '-dark' : ''}.svg\`" alt="Mobile">
                             </td>
                             <td class="fps">
                                 <p>{{ record.hz }}FPS</p>
@@ -98,7 +80,6 @@ export default {
                     <p>(ノಠ益ಠ)ノ彡┻━┻</p>
                 </div>
             </div>
-
             <div class="meta-container">
                 <div class="meta">
                     <div class="errors" v-show="errors.length > 0">
@@ -108,25 +89,42 @@ export default {
                         <h3>List Editors</h3>
                         <ol class="editors">
                             <li v-for="editor in editors">
-                                <img :src="`/assets/${roleIconMap[editor.role]}${store.dark ? '-dark' : ''}.svg`" :alt="editor.role">
+                                <img :src="\`/assets/\${roleIconMap[editor.role]}\${store.dark ? '-dark' : ''}.svg\`" :alt="editor.role">
                                 <a v-if="editor.link" class="type-label-lg link" target="_blank" :href="editor.link">{{ editor.name }}</a>
                                 <p v-else>{{ editor.name }}</p>
                             </li>
                         </ol>
-                    </template>
-
+                                            </template>
                     <h3>Level Records</h3>
-                    <p>Some levels in the list have a different video displayed instead of the first victor's. This is because the original victor's video was either not published publicly or not uploaded at all.</p>
-
+                           <p>
+                        Some levels in the list have a different video displayed instead of the first victor's. This is because the original victor's video was either not published publicly or not uploaded at all.
+                    </p>
+                    </template>
                     <h3>Submission Requirements</h3>
-                    <p>Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)</p>
-                    <p>Achieved the record on the level that is listed on the site - please check the level ID before you submit a record</p>
-                    <p>Have either source audio or clicks/taps in the video. Edited audio only does not count</p>
-                    <p>The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this</p>
-                    <p>The recording must also show the player hit the endwall, or the completion will be invalidated.</p>
-                    <p>Do not use secret routes or bug routes</p>
-                    <p>Do not use easy modes, only a record of the unmodified level qualifies</p>
-                    <p>Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level</p>
+                    <p>
+                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                    </p>
+                    <p>
+                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                    </p>
+                    <p>
+                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                    </p>
+                    <p>
+                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                    </p>
+                    <p>
+                        The recording must also show the player hit the endwall, or the completion will be invalidated.
+                    </p>
+                    <p>
+                        Do not use secret routes or bug routes
+                    </p>
+                    <p>
+                        Do not use easy modes, only a record of the unmodified level qualifies
+                    </p>
+                    <p>
+                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                    </p>
                 </div>
             </div>
         </main>
@@ -141,21 +139,12 @@ export default {
         store
     }),
     computed: {
-        rankedList() {
-            return this.list.slice(0, 150);
-        },
-        legacyList() {
-            return this.list.slice(150);
-        },
-        selectedRank() {
-            return this.selected;
-        },
         level() {
-            return this.list[this.selected]?.[0];
+            return this.list[this.selected][0];
         },
         video() {
-            if (!this.level?.showcase) {
-                return embed(this.level?.verification);
+            if (!this.level.showcase) {
+                return embed(this.level.verification);
             }
 
             return embed(
@@ -166,9 +155,11 @@ export default {
         },
     },
     async mounted() {
+        // Hide loading spinner
         this.list = await fetchList();
         this.editors = await fetchEditors();
 
+        // Error handling
         if (!this.list) {
             this.errors = [
                 "Failed to load list. Retry in a few minutes or notify list staff.",
@@ -191,11 +182,6 @@ export default {
     methods: {
         embed,
         score,
-        selectRanked(i) {
-            this.selected = i;
-        },
-        selectLegacy(j) {
-            this.selected = j + 150;
-        }
     },
 };
+
