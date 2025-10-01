bbf8719ddd9756d85f153f33fa343e5d8c3a854f
JustToxical <justtoxical3940can2023@gmail.com>
2025-09-30 20:28:33 -0400

    Update LevelAuthors.js

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -1,24 +1,14 @@
 export default {
     props: {
-        verifier: {
-            type: String,
-            required: true,
-        },
-        publisher: {
-            type: String,
-            required: true,
-        },
+        verifier: { type: String, required: true },
+        publisher: { type: String, required: true },
     },
     template: `
         <div class="level-authors">
             <div class="type-title-sm">First Victor</div>
-            <p class="type-body">
-                <span>{{ verifier }}</span>
-            </p>
+            <p class="type-body"><span>{{ verifier }}</span></p>
             <div class="type-title-sm">Publisher</div>
-            <p class="type-body">
-                <span>{{ author }}</span>
-            </p>
+            <p class="type-body"><span>{{ author }}</span></p>
         </div>
     `,
 };
