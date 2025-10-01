355572140729734e8506a9e66cf5575dba1a1116
JustToxical <justtoxical3940can2023@gmail.com>
2025-09-30 20:10:45 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -126,10 +126,7 @@ export default {
                     </p>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
-                    </p>
-                    <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                        Achieved the record without using hacks (CBF is allowed on the list)
                     </p>
                     <p>
                         Have either source audio or clicks/taps in the video. Edited audio only does not count
@@ -147,7 +144,7 @@ export default {
                         Do not use easy modes, only a record of the unmodified level qualifies
                     </p>
                     <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        Once a level falls onto the Legacy List, no records will be further accepted onwards.
                     </p>
                 </div>
             </div>
